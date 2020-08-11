37b9ab2b07d3e80adbd92db3467dbde2e058e694
Jens Kutilek <webmail@kutilek.de>
2020-08-11 10:18:36 +0200

    Only match "Name:" when in first line

--- Lib/glyphsLib/builder/features.py
+++ Lib/glyphsLib/builder/features.py
@@ -96,9 +96,7 @@ def _to_ufo_features(font, ufo=None, generate_GDEF=False, skip_export_glyphs=Non
                 name = feature_name.groups()[-1]
                 lines.extend(name.splitlines())
             else:
-                feature_name = re.search(
-                    "^Name: (.+)", feature.notes, flags=re.MULTILINE
-                )
+                feature_name = re.search("^Name: (.+)", feature.notes)
                 if feature_name:
                     name = feature_name.groups()[-1]
                     # Replace special chars backslash and doublequote for AFDKO syntax
